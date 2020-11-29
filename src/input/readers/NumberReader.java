1f4a965fcda07fcc97df4d8ddca4e08ca5db90e8
Beke Ambrus <Ambrus.Beke@kh.hu>
2020-11-29 15:53:36 +0100

    ki kéne számolni, hoyg minden szülejének hány értéke lehet.

--- src/input/readers/NumberReader.java
+++ src/input/readers/NumberReader.java
@@ -3,18 +3,13 @@ package input.readers;
 import java.util.Scanner;
 
 public class NumberReader {
-    public final Scanner consoleScanner = new Scanner(System.in);
-    private final Scanner myInput = new Scanner( consoleScanner.nextLine() );
+    public static final Scanner consoleScanner = new Scanner(System.in);
 
-    public int readInt(){
-        int number = myInput.nextInt();
-        myInput.close();
-        return number;
+    public static int readInt(){
+        return consoleScanner.nextInt();
     }
 
-    public double readDouble(){
-        double number = myInput.nextDouble();
-        myInput.close();
-        return number;
+    public static double readDouble(){
+        return consoleScanner.nextDouble();
     }
 }
