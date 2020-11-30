aec4fe4f268a6766a58a7a1abbffa94c2707b9b2
Beke Ambrus <Ambrus.Beke@kh.hu>
2020-11-30 19:37:30 +0100

    Node beolvasásnál a logika elvoileg jó csak a beolvasás vágja hanyatt magát

--- src/input/readers/NumberReader.java
+++ src/input/readers/NumberReader.java
@@ -1,9 +1,10 @@
 package input.readers;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class NumberReader {
-    public static final Scanner consoleScanner = new Scanner(System.in);
+    public static final Scanner consoleScanner = new Scanner(System.in).useLocale(Locale.US);
 
     public static int readInt(){
         return consoleScanner.nextInt();
